fb236dfebd8e2417991410550a419b5c320bad16
Anton Konyk <antonkonyk.com>
2025-04-15 15:58:26 +0200

    create UserRegistrationRequestSchem

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,6 +1,25 @@
+import re
+
 from pydantic import BaseModel, EmailStr, field_validator
 
 from database import accounts_validators
 
 
-# Write your code here
+class UserRegistrationRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    @field_validator("password", mode="before")
+    @classmethod
+    def validate_password(cls, value: str):
+        if len(value) < 8:
+            raise ValueError("Password must be at least 8 characters long.")
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("Password must contain at least one uppercase letter.")
+        if not re.search(r"[a-z]", value):
+            raise ValueError("Password must contain at least one lowercase letter.")
+        if not re.search(r"\d", value):
+            raise ValueError("Password must contain at least one number.")
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", value):
+            raise ValueError("Password must contain at least one special character.")
+        return value
