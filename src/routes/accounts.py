14db5550997868bd05514c5e1d2a550ac1a5fbb2
Anton Konyk <antonkonyk.com>
2025-04-28 16:06:58 +0200

    remove additional information about error

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -225,11 +225,11 @@ async def password_reset_completion_endpoint(
         await db.commit()
         await db.refresh(db_user)
 
-    except SQLAlchemyError as e:
+    except SQLAlchemyError:
         await db.rollback()
         raise HTTPException(
             status_code=500,
-            detail=f"An error {e} occurred while resetting the password."
+            detail=f"An error occurred while resetting the password."
         )
 
     response_data = MessageResponseSchema(
@@ -274,11 +274,11 @@ async def user_login_endpoint(
         await db.commit()
         await db.refresh(db_user)
 
-    except SQLAlchemyError as e:
+    except SQLAlchemyError:
         await db.rollback()
         raise HTTPException(
             status_code=500,
-            detail=f"An error {e} occurred while processing the request."
+            detail=f"An error occurred while processing the request."
         )
 
     response_data = UserLoginResponseSchema(
